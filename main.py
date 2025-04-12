99db88782f3467ea031de6fe05ac064d09bc12d0
Yilmaz3 <132299159+Yilmaz3@users.noreply.github.com>
2025-04-12 23:39:48 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,12 @@
+# main.py
+import customtkinter as ctk
+from module.gui import TextAdventureGUI  
+
+
+if __name__ == "__main__":
+    ctk.set_appearance_mode("dark")
+    ctk.set_default_color_theme("blue")
+    app = TextAdventureGUI()
+    app.mainloop()
+
+#Bilder und kommentierungen wurden mit ChatGPT erstellt/geschrieben.
